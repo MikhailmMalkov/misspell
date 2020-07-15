bdedd9e6e3db1633000a1df78e457ca4ef8c50fb
Anton Malkov <anton.malkov@gmail.com>
2020-07-15 22:07:11 +0200

    Database created

--- create_db.js
+++ create_db.js
@@ -1,9 +1,13 @@
-var fs = require('fs');
 var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/misspellings_db";
+var url = "mongodb://localhost:27017/";
 
 MongoClient.connect(url, function(err, db) {
   if (err) throw err;
-  console.log("Database created!");
-  db.close();
+  var dbo = db.db("mssp_db");
+  var myobj = { word: "Company Inc", misspelling: "Highway 37" };
+  dbo.collection("words_and_misspellings").insertOne(myobj, function(err, res) {
+    if (err) throw err;
+    console.log("1 document inserted");
+    db.close();
+  });
 });
--- display_exercise.js
+++ display_exercise.js
@@ -60,51 +60,54 @@ fs.readFile('missp.txt', function(err, data) {
       console.log(word_array[w]);
     };
 });
-function display_Exercise(pair){
-  var word_Pair = pair
-  function display_questions(word_Pair){
-    MongoClient.connect(url, function(err, db) {
-      if (err) throw err;
-      var dbo = db.db("msp_db");
-      dbo.createCollection("misspellings", function(err, res) {
-        if (err) throw err;
-        console.log("Collection created!");
-        db.close();
-      });
-    });
 
-    fs.readFile('missp.txt', function(err, data) {
-        if(err) throw err;
-        var line_array = data.toString().split("\n");
-        var word_array = [];
-        var current_word = '';
 
-        for(i in line_array) {
-          var current_spelling = line_array[i];
+// function display_Exercise(pair){
+//   var word_Pair = pair
+//    display_questions(word_Pair){
+//     MongoClient.connect(url, function(err, db) {
+//       if (err) throw err;
+//       var dbo = db.db("msp_db");
+//       dbo.createCollection("misspellings", function(err, res) {
+//         if (err) throw err;
+//         console.log("Collection created!");
+//         db.close();
+//       });
+//     });
+//
+//     fs.readFile('missp.txt', function(err, data) {
+//         if(err) throw err;
+//         var line_array = data.toString().split("\n");
+//         var word_array = [];
+//         var current_word = '';
+//
+//         for(i in line_array) {
+//           var current_spelling = line_array[i];
+//
+//           if (current_spelling[0] === '$') {
+//             current_word = current_spelling.substr(1);
+//           };
+//
+//           if (current_spelling != '' && current_spelling.substr(1) != current_word) {
+//             var entry = {
+//               word: current_word,
+//               misspelling: current_spelling
+//             };
+//             word_array.push(entry);
+//           };
+//         };
+//         var words_Array = []
+//         for(w in word_array) {
+//           for(x in word_array[y]){
+//             words_Array.push(word_Array[w][x])
+//             app.get("/question", function(req, res){
+//               res.send(word_array[w][y] + "\n");
+//             });
+//           };
+//         };
+//     });
+//   };
 
-          if (current_spelling[0] === '$') {
-            current_word = current_spelling.substr(1);
-          };
-
-          if (current_spelling != '' && current_spelling.substr(1) != current_word) {
-            var entry = {
-              word: current_word,
-              misspelling: current_spelling
-            };
-            word_array.push(entry);
-          };
-        };
-        var words_Array = []
-        for(w in word_array) {
-          for(x in word_array[y]){
-            words_Array.push(word_Array[w][x])
-            app.get("/question", function(req, res){
-              res.send(word_array[w][y] + "\n");
-            });
-          };
-        };
-    });
-  };
-app.listen(4000, function(){
-  console.log("Server started on port 4000.")
-});
+// app.listen(4000, function(){
+//   console.log("Server started on port 4000.")
+// });
