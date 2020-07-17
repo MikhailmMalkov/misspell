924291bbae6af10c48ad23ac7afcba6f984c0987
Mike Malkoff <malmishka@gmail.com>
2020-07-17 14:03:16 +0200

    DB data

--- create_db.js
+++ create_db.js
@@ -1,13 +1,42 @@
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/";
+var fs = require('fs');
 
-MongoClient.connect(url, function(err, db) {
-  if (err) throw err;
-  var dbo = db.db("mssp_db");
-  var myobj = { word: "Company Inc", misspelling: "Highway 37" };
-  dbo.collection("words_and_misspellings").insertOne(myobj, function(err, res) {
-    if (err) throw err;
-    console.log("1 document inserted");
-    db.close();
-  });
+fs.readFile('missp.txt', function(err, data) {
+    if(err) throw err;
+    var line_array = data.toString().split("\n");
+    var word_array = [];
+    var current_word = '';
+
+    for(i in line_array) {
+      var current_spelling = line_array[i];
+
+      if (current_spelling[0] === '$') {
+        current_word = current_spelling.substr(1);
+      };
+
+      if (current_spelling != '' && current_spelling.substr(1) != current_word) {
+        var entry = {
+          word: current_word,
+          misspelling: current_spelling
+        };
+        word_array.push(entry);
+      };
+    };
+
+
+
+    MongoClient.connect(url, function(err, db, word_array) {
+      if (err) throw err;
+
+      console.log(word_array);
+
+      var dbo = db.db("missp_db");
+      var myobj = word_array;
+      dbo.collection("words_and_missp").insertMany(myobj, function(err, res) {
+        if (err) throw err;
+        console.log("Number of documents inserted: " + res.insertedCount);
+        db.close();
+      });
+    });
 });
