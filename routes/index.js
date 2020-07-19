0287088525254c7cae7c040e666439c34252645f
Mike Malkoff <malmishka@gmail.com>
2020-07-20 00:03:20 +0200

    Displaying random words to the user.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/";
-/* GET home page. */
+
+
 function make_excercize(sm_array) {
   var questions_array = [];
 
@@ -21,37 +22,44 @@ function make_excercize(sm_array) {
   };
 
   return questions_array;
-
-
 }
 
-MongoClient.connect(url, function(err, db) {
-  if (err) throw err;
-  var dbo = db.db("missp_db");
-  dbo.collection("words_and_missp").find({}).toArray(function(err, result) {
+
+function get_missp_options(exerc_num) {
+
+  MongoClient.connect(url, function(err, db) {
     if (err) throw err;
-    console.log(result);
-    var exercise = make_excercize(result);
-    console.log(exercise);
-    db.close();
-  });
-});
 
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Hello, world!' });
-});
+    var dbo = db.db("missp_db");
+    var coll = dbo.collection('words_and_missp');
 
-router.get('/current_question', function(req, res, next) {
-  res.render('a_question', {
-    title: 'Question x',
-    word_1: "Sample",
-    word_2: "Sample",
-    word_3: "Sample",
-    word_4: "Sample",
-    word_5: "Sample",
-    answer: "Submit",
-    text: "Sample"
+    var cursor = coll.aggregate([{$sample: {size: exerc_num }}]);
+
+    cursor.get(function(err, res){
+      if (err) throw err;
+      var five_pairs = make_excercize(res);
+      router.get('/current_question', function(req, res, next) {
+        res.render('a_question', {
+          title: 'Question x',
+          word_1: five_pairs[0]["option"],
+          word_2: five_pairs[1]["option"],
+          word_3: five_pairs[2]["option"],
+          word_4: five_pairs[3]["option"],
+          word_5: five_pairs[4]["option"],
+          answer: "Submit",
+          text: "Sample"
+        });
+      });
+      db.close();
+    });
   });
-});
+}; // function get_missp_options
+
+// Separate get_missp_options(), make_excercize(), and router.get() from each other and make get_missp_options() more descriptive.
+get_missp_options(5);
+
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Hello, world!' });
+// });
 
 module.exports = router;
