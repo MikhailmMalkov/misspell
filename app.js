e566b466a7d515e208fa772a99c1c2d1ebaafae6
Anton Malkov <anton.malkov@gmail.com>
2020-07-21 23:42:34 +0200

    Return values from the MongoDB database (attempt)

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var mongo = require('mongodb');
+// var mongo = require('mongodb');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var mongodb = require("mongodb");
+// var mongodb = require("mongodb");
 var app = express();
 var fs = require('fs');
 
@@ -39,40 +39,46 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+// DB connect
+var MongoClient = require('mongodb').MongoClient;
+const mongoURL = "mongodb://localhost:27017/";
+const misspellingsDB = "missp_db";
+const misspellingsCollection = "words_and_missp";
+
 
 class Game {
 
   // 1. Константы для: количества заданий, количества вопросов в заданиях, очков за правильный/неправильный ответ.
   // 2. Инициализация новой игры (обнулить счетчик упражнений, баллов)
-  start() {
+  constructor() {
     this.numExercises = 10;
     this.numQuestionsInExercise = 5;
     this.correctAnswerPoints = 1;
     this.incorrectAnswerPoints = -1;
     this.excerciseCounter = 0;
     this.totalPoints = 0;
-  };
+  }
 
   // 3. Вести счетчики упражнений и баллов (методы для обновления счетчиков)
   //      - Увеличить счетчик упражнений на 1
   //      - Увеличить счетчик баллов на N
   updateExcerciseCounter() {
     this.excerciseCounter += 1;
-  };
+  }
 
   updatePoints(num) {
     this.pointCounter += num;
-  };
+  }
 
   // 4. Проверка "закончилась ли игра?"
-  over?() {
+  over() {
     if (this.excerciseCounter >= this.numExercises) {
       return true;
     } else {
       return false;
     };
-  };
-}; // game
+  }
+} // game
 
 
 class Misspellings {
@@ -89,7 +95,7 @@ class Misspellings {
   // 1. Загрузить в БД список слов и мисспеллингов из текстового файла
   importWords() {
 
-  };
+  }
 
   // 2. Почистить БД от "плохих" слов
   сleanDB() {
@@ -98,58 +104,121 @@ class Misspellings {
     //      - убрать географические названия
     //      - убрать имена
     //      - убрать мисспеллинги, которые равны правильным словам
-  };
+  }
 
   //      - убрать дубликаты
   deleteDuplicates(){
 
-  };
+  }
 
   //      - убрать географические названия
   deleteGeo() {
 
-  };
+  }
 
   //      - убрать имена
   deleteProperNames() {
 
-  };
+  }
 
   //      - убрать мисспеллинги, которые равны правильным словам
   deleteCorrectWords() {
 
-  };
+  }
 
   // 3. Достать из БД N случайных слов
   getNWords() {
 
-  };
+  }
 
-}; // misspellings
+} // misspellings
 
 
 class Exercise {
 
   // 1. Создать новое упражнение
   //      - Сформировать задание: 5 слов + правильные/нет?
-  create() {
+  constructor(numWords) {
+    var smArray = this.getWords(numWords);
+    // DB: Как использовать константу из объекта Game с количеством слов в задании?
+    this.exercise = this.makeExcercize(this.words);
+  }
 
-  };
+  showExerciseInConsole() {
+    console.log(this.exercise);
+  }
+
+  // Взять массив из 5 слов+мисспелингов. Сделать массив - слово + правильное/нет
+  makeExcercize(smArray) {
+    var questionsArray = [];
+
+    for (i in smArray) {
+      var question = {};
+      var correct_or_not = Math.random();
+
+      if (correct_or_not > 0.3) {
+        question = {option: smArray[i]['word'], correct: true};
+      } else {
+        question = {option: smArray[i]['misspelling'], correct: false};
+      };
+
+      questionsArray.push(question);
+    };
+
+    return questionsArray;
+  }
+
 
   //      - Достать из БД 5 случайных слов с мисспеллингом
-  getWords() {
+  getWords(numWords) {
+    MongoClient.connect(mongoURL, function(err, db) {
+      if (err) throw err;
 
+      var dbo = db.db(misspellingsDB);
+      var coll = dbo.collection(misspellingsCollection);
+      var res = coll.findOne();
+
+      console.log(res);
+
+      // var cursor = coll.aggregate([{$sample: {size: numWords }}]);
+      //
+      // cursor.on('data', function(data) {
+      //   console.log(data);
+      //   items.push( data );
+      // });
+      //
+      // console.log(items);
+
+      // cursor.toArray(function(err, res){
+      //   if (err) throw err;
+      //   this.words = res;
+      // });
+
+      db.close();
+    });
   }
+
 
   // 2. Вывести упражнение на экран
   show() {
-
+    // router.get('/current_question', function(req, res, next) {
+    //   res.render('a_question', {
+    //     title: 'Question x',
+    //     word_1: five_pairs[0]["option"],
+    //     word_2: five_pairs[1]["option"],
+    //     word_3: five_pairs[2]["option"],
+    //     word_4: five_pairs[3]["option"],
+    //     word_5: five_pairs[4]["option"],
+    //     answer: "Submit",
+    //     text: "Sample"
+    //   });
+    // });
   }
 
   // 3. Получить ответ игрока
   getPlayerAnswer(){
 
-  };
+  }
 
   // 4. Посчитать баллы за одно упражнение
   countAnswerPoints() {
@@ -166,11 +235,18 @@ class Exercise {
 
   }
 
-}; // exercise
+} // exercise
+
+var myExercise = new Exercise(7);
+
+myExercise.showExerciseInConsole();
 
 
-app.listen(4000, function(){
-  console.log("Listening on port 4000")
-});
+// myExercise.showWordsInConsole;
 
-module.exports = app;
+
+// app.listen(4000, function(){
+//   console.log("Listening on port 4000")
+// });
+//
+// module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,63 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/";
-
-function make_excercize(sm_array) {
-  var questions_array = [];
-
-  for (i in sm_array) {
-    var question = {};
-    var correct_or_not = Math.random();
-
-    if (correct_or_not > 0.3) {
-      question = {option: sm_array[i]['word'], correct: true};
-    } else {
-      question = {option: sm_array[i]['misspelling'], correct: false};
-    };
-
-    questions_array.push(question);
-  };
-
-  return questions_array;
-};
-
-
-function get_missp_options(exerc_num) {
-  MongoClient.connect(url, function(err, db) {
-    if (err) throw err;
-
-    var dbo = db.db("missp_db");
-    var coll = dbo.collection('words_and_missp');
-
-    var cursor = coll.aggregate([{$sample: {size: exerc_num }}]);
-
-    cursor.get(function(err, res){
-      if (err) throw err;
-      var five_pairs = make_excercize(res);
-
-      router.get('/current_question', function(req, res, next) {
-        res.render('a_question', {
-          title: 'Question x',
-          word_1: five_pairs[0]["option"],
-          word_2: five_pairs[1]["option"],
-          word_3: five_pairs[2]["option"],
-          word_4: five_pairs[3]["option"],
-          word_5: five_pairs[4]["option"],
-          answer: "Submit",
-          text: "Sample"
-        });
-      });
-    });
-    db.close();
-  });
-}; // function get_missp_options
-
-
-// Separate get_missp_options(), make_excercize(), and router.get() from each other and make get_missp_options() more descriptive.
-get_missp_options(5);
-
 // router.get('/', function(req, res, next) {
 //   res.render('index', { title: 'Hello, world!' });
 // });
